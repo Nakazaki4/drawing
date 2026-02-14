df073647538f858a9d387d02163c7f66e7391f82
Nakazaki4 <nakazakinakazaki22@gmail.com>
2026-02-14 12:04:57 +0100

    drawing line

--- drawing/src/geometrical_shapes.rs
+++ drawing/src/geometrical_shapes.rs
@@ -15,16 +15,61 @@ impl Line {
 
 impl Drawable for Line {
     fn draw(&self, image: &mut Image) {
-        // let c = self.color();
-        // for i in 0..image.width {
-        //     image.display(i, 4, c.clone());
-        // }
+        let x0 = self.start.x;
+        let y0 = self.start.y;
+        let x1 = self.end.x;
+        let y1 = self.end.y;
+
+        let dx = (x1 - x0).abs();
+        let dy = (y1 - y0).abs();
+        let sx = if x0 < x1 { 1 } else { -1 };
+        let sy = if y0 < y1 { 1 } else { -1 };
+
+        let mut tx = x0;
+        let mut ty = y0;
+        let mut idp = 2 * dy - dx;
+        let color = self.color();
+
+        if dx > dy {
+            // Slope < 1
+            loop {
+                image.display(tx, ty, color.clone());
+                if tx == x1 {
+                    break;
+                }
+                tx += sx;
+                if idp > 0 {
+                    ty += sy;
+                    idp += 2 * (dy - dx);
+                } else {
+                    idp += 2 * dy;
+                }
+            }
+        } else {
+            // Slope >= 1
+            idp = 2 * dx - dy;
+            loop {
+                image.display(tx, ty, color.clone());
+                if ty == y1 {
+                    break;
+                }
+                ty += sy;
+                if idp > 0 {
+                    tx += sx;
+                    idp += 2 * (dx - dy);
+                } else {
+                    idp += 2 * dx;
+                }
+            }
+        }
     }
+
     fn color(&self) -> Color {
-        let red: u8 = rand::random_range(0..=255);
-        let green: u8 = rand::random_range(0..=255);
-        let blue: u8 = rand::random_range(0..=255);
-        Color::rgb(red, green, blue)
+        Color::rgb(
+            rand::random_range(0..=255),
+            rand::random_range(0..=255),
+            rand::random_range(0..=255),
+        )
     }
 }
 
